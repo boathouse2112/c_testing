eeafe1e97e1fb2010690313d7429c436f086235e
Mark Murphy <boathouse2112@gmail.com>
2023-05-09 18:38:19 -0400

    Call asm from c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-
+int add_2(int addend);
 
 int main(void)
 {
-    printf("Original: %d, Plus2: %d", 1, 3);
+    int n = 1;
+    int n_plus_2 = add_2(n);
+    printf("Original: %d, Plus2: %d", n, n_plus_2);
+
 	return 0;
 }
