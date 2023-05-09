04e0079e8ab0327c950755527f1d0bfa3000a5e8
Mark Murphy <boathouse2112@gmail.com>
2023-05-09 18:53:47 -0400

    fix

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int printf_asm(char *fmt_string, int number);
 
 void print_args(char *arg1, int arg2) {
-    printf("arg1: %d, arg2: %d\n", arg1, arg2);
+    printf("arg1: %s, arg2: %d\n", arg1, arg2);
 }
 
 int main(void)
@@ -11,6 +11,7 @@ int main(void)
     char *fmt_string = "Your number: %d\n";
     int number = 3;
 
+    printf("arg1: %s, arg2: %d\n", fmt_string, number);
     printf_asm(fmt_string, number);
     printf("Done.");
 
